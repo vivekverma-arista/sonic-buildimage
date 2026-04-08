9685446b8c722051b5b49141947e7d6584753692
Vivek Kumar Verma <vivekverma@arista.com>
2026-04-08 09:52:28 +0000

    Add aristanetworks/sonic as a submodule to platform/aspeed
    Signed-off-by: Vivek Kumar Verma <vivekverma@arista.com>

--- platform/aspeed/rules.mk
+++ platform/aspeed/rules.mk
@@ -1,5 +1,6 @@
 include $(PLATFORM_PATH)/platform-modules-ast-evb.mk
 include $(PLATFORM_PATH)/platform-modules-nexthop.mk
+include $(PLATFORM_PATH)/platform-modules-arista.mk
 include $(PLATFORM_PATH)/one-image.mk
 
 SONIC_ALL += $(SONIC_ONE_IMAGE)
